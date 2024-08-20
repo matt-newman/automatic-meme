c010426b9e3cadb6fbadf815dbf5bf9eeeb0ed93
Matt Newman <matthew.newman@cloudcall.com>
2024-08-20 22:33:15 +0100

    reading the readme, extracting initial thoughts into index file

--- index.ts
+++ index.ts
@@ -11,4 +11,11 @@ export class MarsRover {
     public doThing() {
         return 'result';
     }
+
+    // TODO: controls: 
+    // `L`, `R` and `M` -> left, right and move forward 1 space (at current heading)
+    // Headings: N,E,S,W
+    // Assume that the square directly North from (x, y) is (x, y+1).
+
+    
 }
