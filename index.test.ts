faed7b6051f11ff90a99530e29f7d0287b9bbddb
Matt Newman <matthew.newman@cloudcall.com>
2024-08-20 22:26:50 +0100

    skeleton setup to have working test

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,15 @@
-test("init", () => {
-    expect(false).toBe(true)
+import { MarsRover } from './index';
+
+describe("MarsRover", () => {
+    let program: MarsRover;
+    beforeAll(()=>{
+        program = new MarsRover('');
+    });
+
+    it("should do something", () => {
+        const result = program.doThing();
+        const expected = 'output';
+
+        expect(result).toBe(expected)
+    })
 })
---
+++ index.ts
@@ -0,0 +1,14 @@
+export class MarsRover {
+    private someData = [];
+
+    constructor(parameters) {
+        return this;
+    }
+
+    /**
+     * doThing
+     */
+    public doThing() {
+        return 'result';
+    }
+}
